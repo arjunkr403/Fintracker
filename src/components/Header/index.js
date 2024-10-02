7ccab8d8fca402d0a5c2b91306614272d53ec54c
arjunkr403 <arjunkr403@gmail.com>
2024-10-02 20:59:21 +0530

    Setup complete till firebase

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+const Header = () => {
+  return (
+    <header className="nav">
+      Header
+    </header>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey:import.meta.env.FIREBASE_API_KEY,
+  authDomain: "fintraker.firebaseapp.com",
+  projectId: "fintraker",
+  storageBucket: "fintraker.appspot.com",
+  messagingSenderId: "635249668615",
+  appId:import.meta.env.FIREBASE_API_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db, setDoc, doc,analytics };
