b0dc1477a9fe9672b3ac84ce7e70e252ed22eca2
arjunkr403 <arjunkr403@gmail.com>
2024-10-08 21:03:37 +0530

    created signup page form with hide unhide feature for password , next is to add google auth functionality

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
-function InputBox({label,state,setState,placeholder}) {
+function InputBox({label,state,setState,placeholder,type="text"}) {
   return (
     <div class="input-box">
-      <p>{label}</p>
-      <input value={state} placeholder={placeholder}
-      onChange={(e)=>setState(e.target.value)} required/>
+      <p className="label">{label}</p>
+      <input className="custom" value={state} placeholder={placeholder}
+      onChange={(e)=>setState(e.target.value)} type={type} required/>
     </div>
   );
 }
--- src/components/SignUpIn/index.js
+++ src/components/SignUpIn/index.js
@@ -1,16 +1,70 @@
-import React, { useState } from 'react'
-import './styles.css';
-import InputBox from '../Input';
+import React, { useState } from "react";
+import "./styles.css";
+import InputBox from "../Input";
+import Button from "../Button";
 const SignUpIn = () => {
-  const [name,setName]=useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmP, setConfirmP] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [showConfirmPass, setShowConfirmPass] = useState(false);
+
+  const toggleVisible = () => {
+    setShowPass(!showPass);
+  };
+  const toggleConfirmVisible = () => {
+    setShowConfirmPass(!showConfirmPass);
+  };
   return (
     <div className="signbox">
-      <h2 className='title'>Sign up on <span style={{color:'var(--theme)'}}>FinTracker.</span></h2>
+      <h2 className="title">
+        Sign up on <span style={{ color: "var(--theme)" }}>FinTracker.</span>
+      </h2>
       <form>
-        <InputBox label={"Full Name"} state={name} setState={setName} placeholder={"Your Name"} />
+        <InputBox
+          label={"Full Name"}
+          state={name}
+          setState={setName}
+          placeholder={"John Doe"}
+        />
+        <InputBox
+          label={"Email"}
+          state={email}
+          setState={setEmail}
+          placeholder={"johndoe@gmail.com"}
+        />
+        <div className="pass">
+          <InputBox
+            label={"Password"}
+            state={password}
+            setState={setPassword}
+            type={showPass ? "text" : "password"}
+            placeholder={"example@123"}
+          />
+          <span type="button" onClick={toggleVisible}>
+            {showPass ? "Hide" : "Show"}
+          </span>
+        </div>
+        <div className="pass">
+          <InputBox
+            label={"Confirm Password"}
+            state={confirmP}
+            setState={setConfirmP}
+            type={showConfirmPass ? "text" : "password"}
+            placeholder={"example@123"}
+          />
+          <span type="button" onClick={toggleConfirmVisible}>
+            {showConfirmPass ? "Hide" : "Show"}
+          </span>
+        </div>
+
+        <Button text={'Sign Up using Email and Password'}/>
+        <p style={{textAlign:"center"}}>or</p>
+        <Button  text={'Sign Up using Google'} blue={true}/>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpIn
+export default SignUpIn;
